c12a31685206920aaf2a15a44a934fcade6de89c
Masashi Katsumata <wf9a5m75@gmail.com>
2026-03-23 16:51:55 +0900

    use prebuild sdk

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "mapconductor-for-mapbox",
+    name: "ios-for-mapbox",
     platforms: [
         .iOS(.v17),
     ],
@@ -14,14 +14,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/MapConductor/ios-sdk-core", from: "1.0.0"),
-        .package(url: "https://github.com/mapbox/mapbox-maps-ios", from: "11.0.0"),
+        .package(url: "https://github.com/mapbox/mapbox-maps-ios-binary", from: "11.0.0"),
     ],
     targets: [
         .target(
             name: "MapConductorForMapbox",
             dependencies: [
                 .product(name: "MapConductorCore", package: "ios-sdk-core"),
-                .product(name: "MapboxMaps", package: "mapbox-maps-ios"),
+                .product(name: "MapboxMaps", package: "mapbox-maps-ios-binary"),
             ]
         ),
     ]
